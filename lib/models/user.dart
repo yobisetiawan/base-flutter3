c38324d267e359c6d7bbb0c564549b234c78667a
Yobi Bina Setiawan <yobi.studio@gmail.com>
2022-08-20 22:44:06 +0700

    add user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
 class UserModel {
   UserModel({this.id, this.name, this.email});
 
   String? id;
   String? name;
   String? email;
+
+  /// Connect the generated [_$UserModelFromJson] function to the `fromJson`
+  /// factory.
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  /// Connect the generated [_$UserModelToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
 }
